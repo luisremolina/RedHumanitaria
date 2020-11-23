8c8540b0ee5d09b77f912e117e5b4f302645363e
Luis Remolina <luischuscano1@gmail.com>
2020-11-23 15:30:09 -0500

    Cambiar el idioma a español nativo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nombres');
-            $table->unsignedBigInteger('dni');
+            $table->unsignedBigInteger('dni')->unique();
             $table->string('sexo');
             $table->unsignedBigInteger('telefono');
             $table->unsignedInteger('edad');
