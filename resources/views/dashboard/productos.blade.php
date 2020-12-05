c11d50297ca865366bd037d867544c7cca70cdfc
luisremolina <luischuscano1@gmail.com>
2020-12-04 21:00:28 -0500

    Arregle el fallo extraño, segundo punto de partida

--- resources/views/dashboard/productos.blade.php
+++ resources/views/dashboard/productos.blade.php
@@ -4,45 +4,36 @@
 <div class="container">
 
     <div class="row justify-content-center">
-        <div class="col-1"><a style="width: 100%" href="{{ route('carrito') }}" class="btn btn-warning" role="button" aria-pressed="true"><i class="fa fa-shopping-cart"></i> <span class="badge badge-danger">{{ count($cart)}}</span> </a></div>
-        {{-- /*{{ count($cart)}} --}}
-        <div class="col-md-11">
-            <div class="card">
+        <div class="col-1"><a href="{{ route('carrito') }}" class="btn btn-warning" role="button" aria-pressed="true"><i class="fa fa-shopping-cart"></i> <span class="badge badge-light">{{ count($cart)}}</span> </a></div>
+        <div class="col-md-11 card-header">{{ __('Nuestros Productos') }}</div>
+     
+    </div>
 
-                <div class="card-header">{{ __('Nuestros Productos') }}</div>
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-                  
-                    <div class="row">
-                        @foreach ($productos as $item)
-                        <div class="col-4">
-                            
-                            <img src="{{asset('web/images/coming.jpg')}}" alt="imagen" width="100%" height="300px" class="" style="border-radius:5px;">
+    <div class="row mt-5 justify-content-center">
+                 @foreach ($productos as $item)
 
-                            <h4 class="mt-2 text-center" style="text-transform: capitalize">{{$item->nombre}}</h4>
-                            <p>{{$item->descripcionCorta}}</p>
-                            <div class="badge text-wrap" style="display:flex">
-                                <h4> <span class="badge badge-success"> ${{$item->precioActual}}</span></h4>
-                                <p class="" style="text-decoration: line-through; margin-left: 4px; margin-top: 13px">${{$item->precioAnterior}}</p>
+                        <div class="card-deck col-4 mt-4">
+                            <div class="card">
+                              <img src="{{asset('web/images/coming.jpg')}}" class="card-img-top" alt="foto de algo ">
+                              <div class="card-body">
+                                <h4 style="text-transform: capitalize" class="card-title">{{$item->nombre}}</h4>
+                                <p class="card-text">{{$item->descripcionCorta}}</p>
+                               <div class="d-flex">
+                                        {{-- <p class="card-text "><small class="text-muted"></small></p> --}}
+                                        <h6 class="card-text"> ${{$item->precioActual}} <span class="badge badge-warning">New</span></h6>
+                                        <h6 class="card-text ml-4" style="text-decoration: line-through;"> ${{$item->precioAnterior}}</h6>
+                               </div>
+                                <a style="font-size: 11px" href="{{route('cart', $item->id)}}" class="badge badge-primary">ADD TO CART</a>
+                                <a style="font-size: 11px" href="{{route('detalle', $item->id)}}" class="badge badge-dark">DETALLES</a>
                               </div>
+                            </div>
+                          </div>
 
-                            <a href="{{route('cart', $item->id)}}" class="btn btn-info btn-lg btn-block"
-                                role="button" aria-pressed="true">Agregar al carrito</a>
-                            <a href="{{route('detalle', $item->id)}}" class="btn btn-light btn-lg btn-block"
-                                role="button" aria-pressed="true">Ver detalles</a>
-                            <hr>
-                        </div>
                         @endforeach
 
 
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
+               
+   
 </div>
 @endsection
